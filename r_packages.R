992c5daf79cb8aabb027ca28f7db39f6f3c523e9
Artur Manukyan <artur-man@hotmail.com>
2022-06-13 17:50:08 -0400

    Create r_packages.R

---
+++ r_packages.R
@@ -0,0 +1,16 @@
+# Bioconductor
+install.packages("BiocManager", repos='http://cran.us.r-project.org')
+library(BiocManager)
+
+# Heatmaps
+install.packages("dplyr", repos='http://cran.us.r-project.org')
+install.packages("ComplexHeatmap", repos='http://cran.us.r-project.org')
+install.packages("reshape2", repos='http://cran.us.r-project.org')
+install.packages("circlize", repos='http://cran.us.r-project.org')
+
+# survival analysis for TCGA 
+install.packages("survival", repos='http://cran.us.r-project.org')
+install.packages("survminer", repos='http://cran.us.r-project.org')
+install.packages("RTCGA", repos='http://cran.us.r-project.org')
+install.packages("RTCGA.clinical", repos='http://cran.us.r-project.org')
+install.packages("RTCGA.rnaseq", repos='http://cran.us.r-project.org')
